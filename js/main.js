6f3a6eec52370412dad397c0756a1a18ab8d3677
Davis Li <passionbooster@gmail.com>
2012-09-24 11:49:47 +0800

    initial commit

---
+++ js/main.js
@@ -0,0 +1,31 @@
+// console.log("main.js is loaded"); //Check Chrome Javascript console and you will see this line.
+// $("img").width(200); //jQuery, find img tags in the page and reduce width to 200
+//Highlight the codes, then command or ctrl / and it will be all comments
+
+$(document).ready(function(){
+	/* jQuery document and once document is ready, run this codes in this function
+	ready here means the html webpage is fully loaded. 
+	This is important because jQuery needs to find the tags to execute 
+	So, always write codes inside this block for jQuery */
+	
+	// $("img").on("click", function(){ //same as img.click
+	// 	//when img tag, and when click, do this function
+	// 	//there is a whole list of click, doubleclicks, hover, etc
+	// 	$("img").width(200);
+	// });
+
+//All these functions are found only inside jQuery
+	$("img").on("dblclick", function(){ //same as img.click
+		//when img tag, and when click, do this function
+		//there is a whole list of click, doubleclicks, hover, etc
+		$("img").width(600);
+		$("h1").fadeOut();
+		$(".booboo").slideUp(); //booboo is a class selector in CSS
+		$("#rahul").hide(); //remember rehul is a id tag in CSS
+	});
+
+	// $("img").click(function(){ //find img tag, and once it is clicked, do this this ..
+	// 	$("img").width(200);
+	// }); //img.click
+
+}); //document.ready
